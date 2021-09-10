44df31a37963c27261482260a099d5cf3e091be1
akio0911 <akio0911@gmail.com>
2021-09-10 17:11:25 +0900

    feedback

--- Thema5/Thema5/ViewController.swift
+++ Thema5/Thema5/ViewController.swift
@@ -16,31 +16,23 @@ class ViewController: UIViewController {
         // 入力
         let num1 = Double(textField1.text ?? "") ?? 0
         let num2 = Double(textField2.text ?? "") ?? 0
-        // アラートを作成
-        let title1 = "課題5"
-        let message1 = "割られる数を入力してください"
-        let alert1 = UIAlertController(title: title1, message: message1, preferredStyle: .alert)
-        let title2 = "課題5"
-        let message2 = "割る数を入力してください"
-        let alert2 = UIAlertController(title: title2, message: message2, preferredStyle: .alert)
-        let title3 = "課題5"
-        let message3 = "割る数には0を入力しないでください"
-        let alert3 = UIAlertController(title: title3, message: message3, preferredStyle: .alert)
-        // OKボタンを作成&追加
-        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alert1.addAction(okButton)
-        alert2.addAction(okButton)
-        alert3.addAction(okButton)
-        // アラートを表示
+
         if textField1.text?.isEmpty == true {
-            present(alert1, animated: true, completion: nil)
+            presentAlert(message: "割られる数を入力してください")
         } else if textField2.text?.isEmpty == true {
-            present(alert2, animated: true, completion: nil)
+            presentAlert(message: "割る数を入力してください")
         } else if textField2.text == "0" {
-            present(alert3, animated: true, completion: nil)
+            presentAlert(message: "割る数には0を入力しないでください")
         } else {
             let result = String(num1/num2)
             calculateLabel.text = result
         }
+    }
+
+    private func presentAlert(message: String) {
+        let alert = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
     }
 }
